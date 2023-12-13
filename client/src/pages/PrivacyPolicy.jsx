ec0b85ebed0c8f96ad560d36534a8fdb88d16b59
Aymeric Fraincart <afraincart@aymio.co>
2023-12-13 18:07:52 +0100

    privacy

---
+++ client/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold text-center my-6">Politique de Confidentialité</h1>
+        
+        <section className="my-4">
+            <h2 className="text-2xl font-bold">Introduction</h2>
+            <p>Notre site IA Generator est un site créé dans le cadre d'un apprentissage et s'engage à protéger votre vie privée. Cette politique de confidentialité explique comment nous gérons les informations personnelles et respectons la confidentialité des utilisateurs.</p>
+        </section>
+
+        <section className="my-4">
+            <h2 className="text-2xl font-bold">Collecte des Données</h2>
+            <p>Nous ne collectons pas activement de données personnelles des visiteurs de notre site. Toutefois, certaines informations non personnelles peuvent être recueillies automatiquement pour des raisons de performance et d'analyse du site..</p>
+        </section>
+
+        <section className="my-4">
+            <h2 className="text-2xl font-bold">Utilisation des Données</h2>
+            <p>Les informations collectées automatiquement sont uniquement utilisées pour améliorer l'expérience utilisateur sur notre site et ne sont pas utilisées à des fins commerciales ou partagées avec des tiers..</p>
+        </section>
+
+        <section className="my-4">
+            <h2 className="text-2xl font-bold">Partage des Données</h2>
+            <p>Nous ne partageons, ne vendons et ne divulguons aucune donnée personnelle à des tiers. Les informations non personnelles collectées sont uniquement utilisées pour l'analyse interne..</p>
+        </section>
+
+        <section className="my-4">
+            <h2 className="text-2xl font-bold">Sécurité des Données</h2>
+            <p>Nous prenons des mesures appropriées pour assurer la sécurité des informations collectées. Bien que nous nous efforcions de protéger vos informations, la sécurité absolue ne peut être garantie.</p>
+        </section>
+
+        <section className="my-4">
+            <h2 className="text-2xl font-bold">Droits des Utilisateurs</h2>
+            <p>Comme nous ne collectons pas de données personnelles, les droits habituels tels que l'accès, la rectification et la suppression ne s'appliquent pas dans ce contexte."</p>
+        </section>
+
+        <section className="my-4">
+            <h2 className="text-2xl font-bold">Politique de Cookies</h2>
+            <p>Notre site utilise des cookies pour améliorer l'expérience utilisateur. Ces cookies sont principalement destinés à des fins de performance et d'analyse du site. Vous avez la possibilité de les désactiver dans les paramètres de votre navigateur.</p>
+        </section>
+
+        <section className="my-4">
+            <h2 className="text-2xl font-bold">Utilisation d'AdSense</h2>
+            <p>Nous utilisons Google AdSense pour afficher des publicités. Google peut utiliser des cookies ou des technologies similaires pour fournir des annonces pertinentes. Pour plus d'informations sur la façon dont Google gère les données dans le cadre des annonces, veuillez consulter la politique de confidentialité de Google.</p>
+        </section>
+        <section className="my-4">
+            <h2 className="text-2xl font-bold">Modifications de la Politique de Confidentialité</h2>
+            <p>Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Toute modification sera publiée sur cette page.</p>
+        </section>
+        <section className="my-4">
+            <h2 className="text-2xl font-bold">Contact</h2>
+            <p>Pour toute question concernant notre politique de confidentialité, veuillez nous contacter à afraincart@aymio.co</p>
+        </section>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
